2a1aea92affbf10852616f6baa224f5bb5f76c29
Andrew <BaiganKing@users.noreply.github.com>
2020-06-19 12:53:38 -0600

    removed comment
    Had some unnecessary comments causing clutter

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -3,14 +3,21 @@ Your function should take in a single parameter (a string `word`)
 Your function should return a count of how many occurences of ***"th"*** occur within `word`. Case matters.
 Your function must utilize recursion. It cannot contain any loops.
 '''
+"""
+I need to go through each 2 letters of the sequence
+If those letters are not 'th', I need do the same function, but
+one index forward
+"""
 def count_th(word):
+    # Creating a base case. if there are fewer than 2 letters
+    # They they can't be 'th'
     if len(word) < 2:
         return 0
+    # If they are 'th' add 1 to the count, like the bunny example
     if word[0:2] == 'th':
+        # then use recursion on the whole word, but one index forward
         return 1 + count_th(word[1:])
     else:
+        # Otherwise, I still need to return the recursion,
+        # but without adding 1
         return count_th(word[1:])
-
-    # TBC
-
-    pass
